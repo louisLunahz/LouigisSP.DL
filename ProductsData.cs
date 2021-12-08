218cbdde6b6dc5871a66bec820da8189bb9eae77
louisLunahz <50940896+louisLunahz@users.noreply.github.com>
2021-12-07 18:56:14 -0600

    First commit

---
+++ ProductsData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LouigisSP.BO;
+
+namespace LouigisSP.DL
+{
+   
+    public static class ProductsData
+    {
+        static List<Keyboard> products = new List<Keyboard>();
+
+       
+      //  string distribution, string size, string methodOfConnection, float price, string brand, string ilumination, bool wristRest, string design, string color
+        public static List<Keyboard> getAllProducts() {
+            products.Clear();
+            AddProducts();
+
+            return products;
+        }
+
+        public static void AddProducts() {
+            Keyboard k1 = new Keyboard("190896", "Spanish", "80%", "USB", 123, "Hyper X", "alloy fps", "RGB", false, "QWERTY", "red");
+            Keyboard k2 = new Keyboard("190897", "English", "100%", "USB", 143, "Elecom", "TK-FDM063TBK", "No", false, "QWERTY", "black");
+            Keyboard k3 = new MechanicalKeyboard("190898", "Spanish", "100%", "USB", 200, "Hyper X", "HX-KB1SS2-LA", "RGB", false, "QWERTY", "black", "red", "Cherry MX");
+
+            products.Add(k1);
+            products.Add(k2);
+            products.Add(k3);
+
+        }
+
+        
+
+    }
+}
---
+++ UsersData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LouigisSP.BO;
+using System.Collections.Generic;
+
+namespace LouigisSP.DL
+{
+    public static class UsersData
+    {
+
+         static List<Person> persons = new List<Person>();
+        public static List<Person> GetAllUsers()
+        {
+            persons.Clear();
+            Person emp = new Employee(123, "jose", "Luna", "2221989689", "louislunahz@gmail.com", "123123", new DateTime(2010,03,03), new DateTime(1987,12,01));
+            Person emp2 = new Employee(124, "Carl", "Jonson", "2211207149", "carljohnson@gmail.com", "12345", new DateTime(1997,12,12), DateTime.Now);
+            Person customer = new Customer(124, "Tyler", "Durden", "2212323445", "tylerdurden@gmail.com", "password", new DateTime(1996-08-19), DateTime.Now, "Ignacio Allende 12911 Guadalupe HIdalgo", "Ignacio Allende 12991 Guadalupe HIdalgo");
+            persons.Add(emp);
+            persons.Add(emp2);
+            persons.Add(customer);
+
+            return persons;
+        }
+
+        public static bool AddPerson(Person person) {
+            if (person != null)
+            {
+                persons.Add(person);
+                return true;
+            }
+            else return false;  
+            
+            
+
+        }
+
+
+    }
+}
